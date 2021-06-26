63bec0007b3460273420c56164ceb7b4975f47bb
polatengin <enpolat@microsoft.com>
2021-06-26 14:25:05 +0300

    user api project created

---
+++ src/api-user/Startup.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace api_user
+{
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    string[] words = { "lorem", "ipsum", "dolor", "sit", "amet", "consectetuer", "adipiscing", "elit", "sed", "diam", "nonummy", "nibh", "euismod", "tincidunt", "ut", "laoreet", "dolore", "magna", "aliquam", "erat" };
+    Random random = new Random();
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseRouting();
+
+      app.UseEndpoints(endpoints =>
+      {
+      });
+    }
+  }
+}
