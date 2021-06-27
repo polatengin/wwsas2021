f11d4f60c552ed3f6ab692161f6efa87e4725bdd
polatengin <enpolat@microsoft.com>
2021-06-27 14:26:53 +0300

    check if required azure cli is installed, if it's not, install it

--- iac/0_setup.sh
+++ iac/0_setup.sh
@@ -1,2 +1,9 @@
 PROJECT_NAME="wwsas2021"
 LOCATION="northeurope"
+
+AZURE_CLI_EXISTS=$(command -v "az")
+if [[ -z ${AZURE_CLI_EXISTS} ]]; then
+  echo "Installing Azure CLI..."
+
+  curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+fi
