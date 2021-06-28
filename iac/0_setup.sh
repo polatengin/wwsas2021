914b4639de5e4311585788f6185d6825b8828584
polatengin <enpolat@microsoft.com>
2021-06-28 22:10:47 +0300

    checked if github cli installed, if not, install...

--- iac/0_setup.sh
+++ iac/0_setup.sh
@@ -8,6 +8,16 @@ if [[ -z ${AZURE_CLI_EXISTS} ]]; then
   curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 fi
 
+GITHUB_CLI_EXISTS=$(command -v "gh")
+if [[ -z ${GITHUB_CLI_EXISTS} ]]; then
+  echo "Installing GitHub CLI..."
+
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+  apt update
+  apt install gh
+fi
+
 LOGIN_COUNT=`az account list --query "length([])"`
 
 if [[ ${LOGIN_COUNT} -eq 0 ]]
