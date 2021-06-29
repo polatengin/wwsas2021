7a68ee43bbf82558e8fef1f5a0b583e16982080c
polatengin <enpolat@microsoft.com>
2021-06-29 08:57:10 +0300

    unneeded set arguments removed

--- iac/0_setup.sh
+++ iac/0_setup.sh
@@ -71,11 +71,8 @@ kubectl create namespace ingress-basic
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 
 helm install nginx-ingress ingress-nginx/ingress-nginx \
-    --namespace ingress-basic \
-    --set controller.replicaCount=2 \
-    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
-    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
-    --set controller.admissionWebhooks.patch.nodeSelector."beta\.kubernetes\.io/os"=linux
+  --namespace ingress-basic \
+  --set controller.replicaCount=2
 
 echo "Configuring nginx"
 
