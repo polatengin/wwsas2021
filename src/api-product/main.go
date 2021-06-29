f015689272978eb5c21e0cae2b75667d14da660a
polatengin <enpolat@microsoft.com>
2021-06-29 08:58:02 +0300

    simple http server implementation for product list

--- src/api-product/main.go
+++ src/api-product/main.go
@@ -1,7 +1,35 @@
-package wwsas2021
+package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Product struct {
+	Name            string
+	Price           float32
+	InStock         bool
+	CampaignStartOn time.Time
+}
+type ProductList []Product
 
 func main() {
-	fmt.Println("hello wwsas2021")
+	http.HandleFunc("/get-product-list", func(w http.ResponseWriter, r *http.Request) {
+		productList := ProductList{
+			Product{Name: "LED TV"},
+			Product{Name: "Coffee Maker"},
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(productList)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world!")
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
