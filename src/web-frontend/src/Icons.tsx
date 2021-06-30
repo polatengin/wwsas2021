36209f7d2dbe3543e7c5df049bb6a0d2c98a9f4d
polatengin <enpolat@microsoft.com>
2021-06-30 18:52:29 +0300

    adding tag icon

--- src/web-frontend/src/Icons.tsx
+++ src/web-frontend/src/Icons.tsx
@@ -48,3 +48,11 @@ export const UserIcon: FunctionComponent<IconProps> = (props: IconProps) => {
     </svg>
   );
 };
+
+export const TagIcon: FunctionComponent<IconProps> = (props: IconProps) => {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" className={props.className} onClick={props.onClick}>
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
+    </svg>
+  );
+};
