e60fb490802eaa63ad1b5e8da53fbce41810041d
polatengin <enpolat@microsoft.com>
2021-06-30 13:44:44 +0300

    adding logo icon

--- src/web-frontend/src/App.tsx
+++ src/web-frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import React, { FunctionComponent } from 'react';
 
+import { StoreLogoIcon } from './Icons';
+
 export const App: FunctionComponent = () => {
   return (
     <>
       <header className="">
+        <StoreLogoIcon />
       </header>
       <main className="">
       </main>
