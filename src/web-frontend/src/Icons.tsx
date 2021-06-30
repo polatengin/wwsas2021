93d2003714a9828c3c0b0cccb57ebb03db8fae32
polatengin <enpolat@microsoft.com>
2021-06-30 17:21:33 +0300

    adding shopping cart icon

--- src/web-frontend/src/Icons.tsx
+++ src/web-frontend/src/Icons.tsx
@@ -32,3 +32,11 @@ export const StoreLogoIcon: FunctionComponent<IconProps> = (props: IconProps) =>
     </svg>
   );
 };
+
+export const ShoppingCartIcon: FunctionComponent<IconProps> = (props: IconProps) => {
+  return (
+    <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" className={props.className} onClick={props.onClick}>
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+    </svg>
+  );
+};
