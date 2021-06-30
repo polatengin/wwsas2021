c21b7699a43ab0b79cd549c59a9fe13af25fa350
polatengin <enpolat@microsoft.com>
2021-06-30 13:41:09 +0300

    adding base html

--- src/web-frontend/src/App.tsx
+++ src/web-frontend/src/App.tsx
@@ -3,7 +3,12 @@ import React, { FunctionComponent } from 'react';
 export const App: FunctionComponent = () => {
   return (
     <>
+      <header className="">
       </header>
+      <main className="">
+      </main>
+      <footer className="">
+      </footer>
     </>
   );
 };
