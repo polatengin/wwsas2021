66fcab1269f7389663e984f70f4ab875be3a2f8a
polatengin <enpolat@microsoft.com>
2021-06-30 17:22:15 +0300

    updating base html

--- src/web-frontend/src/App.tsx
+++ src/web-frontend/src/App.tsx
@@ -1,17 +1,22 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
 
-import { StoreLogoIcon } from './Icons';
+import { ShoppingCartIcon, StoreLogoIcon, UserIcon } from './Icons';
 
 export const App: FunctionComponent = () => {
   return (
-    <>
-      <header className="">
-        <StoreLogoIcon />
+    <div className="flex flex-col bg-gray-300 h-3/4 px-4 rounded-xl">
+      <header className="flex my-4 items-center">
+        <StoreLogoIcon className="w-6 h-6 mr-2" />
+        <span className="text-lg">WorldWide Software Architecture Summit</span>
+        <span className="flex-grow"></span>
+        <ShoppingCartIcon className="w-6 h-6" />
+        <UserIcon className="w-6 h-6" />
       </header>
-      <main className="">
+      <main className="flex-grow">
+        adasd
       </main>
-      <footer className="">
+      <footer className="my-4">
       </footer>
-    </>
+    </div>
   );
 };
