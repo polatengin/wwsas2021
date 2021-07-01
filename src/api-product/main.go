3456e5d6031f8f88f9efd041c453921bda881cdd
polatengin <enpolat@microsoft.com>
2021-07-01 13:40:06 +0300

    adding a new product

--- src/api-product/main.go
+++ src/api-product/main.go
@@ -48,6 +48,13 @@ func main() {
 				Rating:   5,
 				ImageUrl: fmt.Sprintf("https://picsum.photos/100?color&random=%d", rand.Intn(99999)),
 			},
+			Product{
+				Name:     "GoMacro Macrobar Protein Paradise, Cashew Caramel",
+				Price:    25.75,
+				InStock:  true,
+				Rating:   5,
+				ImageUrl: fmt.Sprintf("https://picsum.photos/100?color&random=%d", rand.Intn(99999)),
+			},
 		}
 
 		w.Header().Set("Content-Type", "application/json")
